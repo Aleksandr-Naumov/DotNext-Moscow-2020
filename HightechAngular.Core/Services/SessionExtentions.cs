d55015a1720cdc18d68cefe0d648b3280a144048
cmekoneup <aleksandr_naumov_01@mail.ru>
2021-02-14 10:06:34 +0300

    [delete]commented out

--- HightechAngular.Core/Services/SessionExtentions.cs
+++ HightechAngular.Core/Services/SessionExtentions.cs
@@ -8,14 +8,12 @@ namespace HightechAngular.Orders.Services
     {
         public static void Set<T>(this ISession session, string key, T value)
         {
-            //session.SetString(key, JsonSerializer.Serialize(value));
             session.SetString(key, JsonConvert.SerializeObject(value));
         }
 
         public static T Get<T>(this ISession session, string key)
         {
             var value = session.GetString(key);
-            //return value == null ? default : JsonSerializer.Deserialize<T>(value);
             return value == null ? default : JsonConvert.DeserializeObject<T>(value);
         }
     }
